734c84c19ea7e4adf5f645cc58ad0ef22d7c7e92
SabrinaHF <sabrinahoflor@gmail.com>
2024-10-09 16:30:28 -0300

    Alteração

--- aula06/Lista_Exercícios/questao04.py
+++ aula06/Lista_Exercícios/questao04.py
@@ -1,3 +1,10 @@
 # Armazene os nomes de alguns de seus amigos em uma lista chamada amigos. Exiba o nome de cada pessoa acessando cada 
 # elemento da lista um de cada vez.
 
+amigos = ['Emanoel','Priscilla','Bárbara']
+
+for i in amigos:
+    
+    print(f'Migs: {i}')
+    
+    
--- aula06/Lista_Exercícios/questao05.py
+++ aula06/Lista_Exercícios/questao05.py
@@ -1,2 +1,8 @@
 # Utilizando a lista do exercício anterior, exiba uma saudação ("Olá como vai você"), 
 # personalizado com o nome de cada amigo. A saudação deve ser a mesma, alterando apenas o nome do amigo.
+
+amigos = ['Emanoel','Priscilla','Bárbara']
+
+for i in amigos:
+    
+    print(f'Olá como vai você, {i}?')
